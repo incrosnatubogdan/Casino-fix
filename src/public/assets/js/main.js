61c07dac5c625edbb7d0ef760798f9dcf1af2f5e
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2019-07-26 00:24:15 +0300

    game section

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log(123)
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+    })
+});
